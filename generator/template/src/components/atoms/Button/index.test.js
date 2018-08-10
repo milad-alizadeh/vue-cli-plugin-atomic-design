3121f3ffc7a37025ce556ddb83f701e6bf410059
Milad Alizadeh <milaadd@gmail.com>
2018-08-10 13:12:14 +0100

    remove sample code

--- generator/template/src/components/atoms/Button/index.test.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import Button from './index.vue'
-
-describe('Atom - Button', () => {
-  test('it renders', () => {
-    const wrapper = mount(Button)
-    expect(wrapper.html()).toContain('<button>Button</button>')
-  })
-})
