cd2ad977752a8c2d392642c0fb7358da52d5ae93
Milad Alizadeh <milaadd@gmail.com>
2018-08-10 13:12:06 +0100

    add prompt

---
+++ prompts.js
@@ -0,0 +1,8 @@
+module.exports = [
+  {
+    name: 'type',
+    type: 'confirm',
+    message: 'WARNING! This plugin will alter your existing project structure. please commit your files before continuing and when ready press "Y"?',
+    default: true
+  },
+];
