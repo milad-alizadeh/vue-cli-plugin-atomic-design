a9b547054960eb20643230502eea72a0d1168af7
Milad Alizadeh <milaadd@gmail.com>
2018-08-14 13:53:02 +0100

    move page folder to components

--- generator/template/src/pages/index.js
+++
@@ -1,9 +0,0 @@
-const req = require.context('.', false, /^((?!index).)*\.js$/)
-const containers = {}
-
-req.keys().forEach(key => {
-  const containerName = key.replace(/^\.\/([^.]+)\.js$/, '$1')
-  containers[containerName] = req(key).default
-})
-
-export default containers
