741dde9137b2e7b29cc9c5a3f336dbff35aa79f9
Milad Alizadeh <milaadd@gmail.com>
2018-08-14 13:52:48 +0100

    update page components

--- generator/template/src/components/index.js
+++ generator/template/src/components/index.js
@@ -1,9 +1,7 @@
+import Vue from 'vue'
 const req = require.context('.', true, /\.\/[^/]+\/[^/]+\/index\.vue$/)
-const components = {}
 
 req.keys().forEach(key => {
   const componentName = key.replace(/^.+\/([^/]+)\/index\.vue/, '$1')
-  components[componentName] = req(key).default
+  Vue.component(componentName, req(key).default)
 })
-
-export default components
