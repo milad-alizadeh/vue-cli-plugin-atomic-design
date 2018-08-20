f2a2f1e780269743f6e69629773914bbf1b6e2cc
Milad Alizadeh <milaadd@gmail.com>
2018-08-20 16:22:23 +0100

    cahnge component alias

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = (api, projectOptions) => {
     return {
       resolve: {
         alias: {
-          components: path.resolve(__dirname, '../../src/components'),
+          components: path.resolve(__dirname, '../../private/storybook/components'),
           atoms: path.resolve(__dirname, '../../src/components/atoms/'),
           molecules: path.resolve(__dirname, '../../src/components/molecules/'),
           organisms: path.resolve(__dirname, '../../src/components/organisms/'),
