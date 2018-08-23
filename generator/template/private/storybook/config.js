71697bc4d37b69bc115bb6ed3f2796225e0a8d5d
Milad Alizadeh <milaadd@gmail.com>
2018-08-23 16:48:14 +0100

    add babel polyfill to storybook

--- generator/template/private/storybook/config.js
+++ generator/template/private/storybook/config.js
@@ -1,3 +1,4 @@
+import 'babel-polyfill'
 import { configure, addDecorator } from '@storybook/vue'
 import { setOptions } from '@storybook/addon-options'
 import StoryRouter from 'storybook-vue-router'
