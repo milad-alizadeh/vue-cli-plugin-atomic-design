67bbccec6944a59de100475d805346b66bcb4997
Milad Alizadeh <milaadd@gmail.com>
2018-10-02 16:35:21 +0100

    remove unnecessary alias

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ module.exports = (api, projectOptions) => {
     return {
       resolve: {
         alias: {
-          components: path.resolve(__dirname, '../../private/storybook/components'),
           atoms: path.resolve(__dirname, '../../src/components/atoms/'),
           molecules: path.resolve(__dirname, '../../src/components/molecules/'),
           organisms: path.resolve(__dirname, '../../src/components/organisms/'),
