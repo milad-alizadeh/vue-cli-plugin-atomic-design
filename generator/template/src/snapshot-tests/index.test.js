80712f5ae28aa2ce25c3056c584d459369670792
Milad Alizadeh <milaadd@gmail.com>
2018-10-17 15:32:21 +0100

    update snapshot tests

--- generator/template/src/snapshot-tests/index.test.js
+++ generator/template/src/snapshot-tests/index.test.js
@@ -7,34 +7,40 @@ import routes from './routes'
 expect.extend({ toMatchImageSnapshot })
 
 describe('Screenshot tests', () => {
-  let page
-  beforeAll(async () => {
-    const browser = await puppeteer.launch({ headless: true })
-    page = await browser.newPage()
-  })
-
-  routes.forEach(({ path, name }) => {
-    test(`${name} page - mobile screenshot`, async () => {
-      await page.goto(PATH + path, { waitUntil: 'load' })
-
-      page.setViewport({
-        width: 320,
-        height: 480
-      })
-
-      let screen = await page.screenshot({ fullPage: true })
-
-      expect(screen).toMatchImageSnapshot()
+  if (routes.length) {
+    let page
+    beforeAll(async () => {
+      const browser = await puppeteer.launch({ headless: true })
+      page = await browser.newPage()
     })
 
-    test(`${name} page - desktop screenshot`, async () => {
-      page.setViewport({
-        width: 800,
-        height: 600
+    routes.forEach(({ path, name }) => {
+      test(`${name} page - mobile screenshot`, async () => {
+        await page.goto(PATH + path, { waitUntil: 'load' })
+
+        page.setViewport({
+          width: 320,
+          height: 480
+        })
+
+        let screen = await page.screenshot({ fullPage: true })
+
+        expect(screen).toMatchImageSnapshot()
       })
 
-      let screen = await page.screenshot({ fullPage: true })
-      expect(screen).toMatchImageSnapshot()
+      test(`${name} page - desktop screenshot`, async () => {
+        page.setViewport({
+          width: 800,
+          height: 600
+        })
+
+        let screen = await page.screenshot({ fullPage: true })
+        expect(screen).toMatchImageSnapshot()
+      })
     })
-  })
+  } else {
+    test('Screenshot test', () => {
+      console.log('no route is defined for screenshot test')
+    })
+  }
 })
--- generator/template/src/snapshot-tests/routes.js
+++ generator/template/src/snapshot-tests/routes.js
@@ -1,11 +1,10 @@
+/**
+ * An array of routes for screenshot tests
+ */
 export default [
   // {
   //   path: '/',
   //   name: 'Home'
-  // },
-  // {
-  //   path: '/about',
-  //   name: 'About'
   // },
   // {
   //   path: '/contact',
