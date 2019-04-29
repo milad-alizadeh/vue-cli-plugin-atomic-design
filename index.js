51e4b998b089217a3bfbbaa77ff568ec08f48588
Milad Alizadeh <milaadd@gmail.com>
2019-04-29 11:43:50 +0100

    remove storybook files

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const server = require('@storybook/core/server')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const path = require('path')
-const wrapInitialConfig = require('./wrapInitialConfig')
 
 // eslint-disable-next-line no-unused-vars
 module.exports = (api, { pluginOptions = {} }) => {
@@ -37,75 +35,4 @@ module.exports = (api, { pluginOptions = {} }) => {
      })
    )
   })
-
-  // Set up Storybook commands
-
-  /**
-   * This section is owed to pksunkara
-   * https://github.com/pksunkara/vue-cli-plugin-storybook
-   */
-
-  const wrapDefaultConfig = config => ({
-    ...config,
-    module: {
-      ...config.module,
-      rules: config.module.rules.slice(0, -4),
-    },
-  })
-
-  const defaultOptions = {
-    allowedPlugins: [],
-  }
-
-  const options = Object.assign({}, defaultOptions, pluginOptions.storybook)
-
-  api.registerCommand('serve:storybook', {
-    description: 'Start storybook',
-    usage: 'vue-cli-service serve:storybook',
-    options: {
-      '-p, --port [number]': 'Port to run Storybook (required)',
-      '-h, --host [string]': 'Host to run Storybook',
-      '-s, --static-dir <dir-names>': 'Directory where to load static files from',
-      '-c, --config-dir [dir-name]': 'Directory where to load Storybook configurations from',
-      '--https': 'Serve Storybook over HTTPS. Note: You must provide your own certificate information.',
-      '--ssl-ca <ca>': 'Provide an SSL certificate authority. (Optional with --https, required if using a self-signed certificate)',
-      '--ssl-cert <cert>': 'Provide an SSL certificate. (Required with --https)',
-      '--ssl-key <key>': 'Provide an SSL key. (Required with --https)',
-      '--smoke-test': 'Exit after successful start',
-      '--quiet': 'Suppress verbose build output',
-    },
-  }, () => {
-    server.buildDev({
-      packageJson: {
-        name: '@storybook/vue',
-        version: '4.0.0-alpha.20',
-      },
-      wrapInitialConfig: wrapInitialConfig(api, options),
-      wrapDefaultConfig,
-    })
-  })
-
-  api.registerCommand('build:storybook', {
-    description: 'Build storybook',
-    usage: 'vue-cli-service build:storybook',
-    options: {
-      '-s, --static-dir <dir-names>': 'Directory where to load static files from',
-      '-o, --output-dir [dir-name]': 'Directory where to store built files',
-      '-c, --config-dir [dir-name]': 'Directory where to load Storybook configurations from',
-      '-w, --watch': 'Enable watch mode (default: false)'
-    },
-  }, () => {
-    server.buildStatic({
-      packageJson: {
-        name: '@storybook/vue',
-        version: '4.0.0-alpha.20',
-      },
-      wrapInitialConfig: wrapInitialConfig(api, options),
-      wrapDefaultConfig
-    })
-  })
-}
-
-module.exports.defaultModes = {
-  'build:storybook': 'production',
 }
